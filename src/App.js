6b3974aa7e5e8e65ad76a5da08f42576cfd918b0
SankhaYapa <sankhapriyamantha55@gmail.com>
2023-03-12 00:55:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { Dashboard } from "./dashboard/Dashboard";
+import { ViewWhether } from "./viewWhether/ViewWhether";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route exact path="/" element={<Dashboard></Dashboard>}></Route>
+    <Route path="/viewWeather/name=?" element={<ViewWhether />} />
+    
+     
+    </Routes>
+    </BrowserRouter>
   );
 }
 
